44fd8e3eb5cd53eb85934d3d84bda7829cbf8736
Haibin Lin <linhaibin.eric@gmail.com>
2018-10-10 21:56:36 -0700

    Update train_resnet50_imagenet.sh

--- models/resnet/mxnet/train_resnet50_imagenet.sh
+++ models/resnet/mxnet/train_resnet50_imagenet.sh
@@ -10,4 +10,4 @@ git clone --recursive https://github.com/apache/incubator-mxnet
 # Ensure data is kept at ~/data for each machine or change the data paths below
 # Example command to start the training job
 # Specify hosts in the file `hosts`
-incubator-mxnet/tools/launch.py -n 8 -H hosts python train_imagenet.py --use-rec --batch-size 256 --dtype float16 --num-data-workers 40 --num-epochs 90 --gpus 0,1,2,3,4,5,6,7 --lr 6.4 --lr-mode poly --warmup-epochs 10 --last-gamma --mode symbolic --model resnet50_v1b --kvstore dist_sync_device --rec-train ~/data/train.rec --rec-train-idx ~/data/train.idx --rec-val ~/data/val.rec --rec-val-idx ~/data/val.idx --save_frequency 1000 --warmup-lr 0.001 2>&1 | tee resnet50.log
+incubator-mxnet/tools/launch.py -n 8 -H hosts python train_imagenet.py --use-rec --batch-size 256 --dtype float16 --num-data-workers 40 --num-epochs 90 --gpus 0,1,2,3,4,5,6,7 --lr 6.4 --lr-mode poly --warmup-epochs 10 --last-gamma --mode symbolic --model resnet50_v1b --kvstore dist_sync_device --rec-train ~/data/train.rec --rec-train-idx ~/data/train.idx --rec-val ~/data/val.rec --rec-val-idx ~/data/val.idx --save-frequency 1000 --warmup-lr 0.001 2>&1 | tee resnet50.log
