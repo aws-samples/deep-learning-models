e5bfa245ddf73ac10ad1d0a2f882aa04277d556e
Rahul Huilgol <huilgolr@amazon.com>
2018-11-08 14:15:35 -0800

    fix directory change

--- models/resnet/tensorflow/dlami_scripts/train_64gpus.sh
+++ models/resnet/tensorflow/dlami_scripts/train_64gpus.sh
@@ -8,7 +8,7 @@
 # If you are using AmazonLinux change ens3 in line 15 to eth0
 # If you have version 12 or older, you need to remove line 15 below.
 source activate tensorflow_p36
-
+cd ..
 /home/ubuntu/anaconda3/envs/tensorflow_p36/bin/mpirun -np 64 -hostfile hosts -mca plm_rsh_no_tree_spawn 1 \
 	-bind-to socket -map-by slot \
 	-x HOROVOD_HIERARCHICAL_ALLREDUCE=1 -x HOROVOD_FUSION_THRESHOLD=16777216 \
