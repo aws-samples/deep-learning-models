82168b8bb5a02ab3530a904da2e11b943adb483d
haohanchen-yagao <haohanchen1994@gmail.com>
2020-05-13 14:56:07 -0700

    add albert ci

---
+++ ci/frcnn/run_singlenode.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ci
+echo start launching
+#python launch_ec2.py 2>&1 | tee log.txt
+#python launch_ec2.py 2> /dev/null
+#echo start parsing
+#python parse_and_submit.py log.txt  8 2 p3.16xlarge EC2
