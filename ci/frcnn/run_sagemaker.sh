2ba35aa03b02ec2d5f3b28389a42e45910455f0c
haohanchen-yagao <haohanchen1994@gmail.com>
2020-05-13 14:57:27 -0700

    minor fix

--- ci/frcnn/run_sagemaker.sh
+++ ci/frcnn/run_sagemaker.sh
@@ -4,5 +4,5 @@
 echo start launching
 #python launch_sagemaker.py | tee log.txt
 python launch_sagemaker.py > log.txt
-#echo start parsing
+echo start parsing
 python ci/parse_and_submit.py log.txt 8 2 p3.16xlarge Sagemaker
