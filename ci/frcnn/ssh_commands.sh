180732838df52d0fea3176b0e7637e0541091445
haohanchen-yagao <54413235+haohanchen-yagao@users.noreply.github.com>
2020-05-15 12:17:05 -0700

    Add ci implementation for faster-rcnn (#14)
    * add frcnn implementation
    
    * fix typo
    
    * stop instances after building
    
    * run container first

--- ci/frcnn/ssh_commands.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker stop tensorflow
-docker run --gpus all -it --rm -d --net=host --name tensorflow -v ~/shared_workspace/:/workspace/shared_workspace johnbensnyder/ngc:20.02 bash
-docker exec tensorflow /bin/sh -c 'cd shared_workspace/mmdetection_tf; mpirun -np 8 --host localhost:8 --allow-run-as-root python /workspace/shared_workspace/mmdetection_tf/train_hvd.py' > log.txt
-python parse_and_submit.py log.txt  8 2 p3.16xlarge EC2
