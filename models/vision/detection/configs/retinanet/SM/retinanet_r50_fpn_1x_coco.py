119226ee33db3488530557c1bce429b8e19ee4a7
Anurag Singh <anuragrs@gmail.com>
2020-06-25 22:03:22 +0000

    added SM config for retinanet r50 fpn 1x

--- models/vision/detection/configs/retinanet/SM/retinanet_r50_fpn_1x_coco.py
+++ models/vision/detection/configs/retinanet/SM/retinanet_r50_fpn_1x_coco.py
@@ -2,14 +2,52 @@
 # SPDX-License-Identifier: Apache-2.0
 # -*- coding: utf-8 -*-
 
+# date time settings to update paths for jobs
+from datetime import datetime
+now = datetime.now()
+time_str = now.strftime("%d-%m-%Y-%H-%M")
+date_str = now.strftime("%d-%m-%Y")
+
+
+# sagemaker settings
+sagemaker_user=dict(
+    user_id='mzanur',
+    s3_bucket='mzanur-sagemaker',
+    docker_image='578276202366.dkr.ecr.us-east-1.amazonaws.com/mzanur-awsdet-ecr:awsdet',
+    hvd_processes_per_host=8,
+    hvd_instance_type='ml.p3dn.24xlarge', # 'ml.p3.16xlarge',
+    hvd_instance_count=2, #1,
+)
+# settings for distributed training on sagemaker
+distributions=dict(
+    mpi=dict(
+        enabled=True,
+        processes_per_host=sagemaker_user['hvd_processes_per_host'],
+        custom_mpi_options="-x OMPI_MCA_btl_vader_single_copy_mechanism=none -x TF_CUDNN_USE_AUTOTUNE=0",
+    )
+)
+# sagemaker channels
+channels=dict( 
+    coco='s3://{}/awsdet/data/coco/'.format(sagemaker_user['s3_bucket']),
+    weights='s3://{}/awsdet/data/weights/'.format(sagemaker_user['s3_bucket'])
+)
+
+sagemaker_job=dict(
+    s3_path='s3://{}/retinanet/outputs/{}'.format(sagemaker_user['s3_bucket'], time_str),
+    job_name='{}-retinanet-{}'.format(sagemaker_user['user_id'], time_str),
+    output_path='',
+)
+sagemaker_job['output_path']='{}/output/{}'.format(sagemaker_job['s3_path'], sagemaker_job['job_name'])
+
+
 # model settings
 model = dict(
     type='RetinaNet',
     pretrained=None,
     backbone=dict(
         type='KerasBackbone',
-        model_name='ResNet50V1_AWS',
-        weights_path='weights/resnet50', # SavedModel format
+        model_name='ResNet50V1',
+        weights_path='weights/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5',
         weight_decay=5e-5
     ),
     neck=dict(
@@ -19,7 +57,7 @@ model = dict(
         start_level=1,
         add_extra_convs=True,
         num_outs=5,
-        interpolation_method='bilinear', #'nearest',
+        interpolation_method='bilinear',
         weight_decay=5e-5,
     ),
     bbox_head=dict(
@@ -64,9 +102,9 @@ data = dict(
         subset='train',
         flip_ratio=0.5,
         pad_mode='fixed',
-        preproc_mode='rgb',
-        mean=(123.68, 116.78, 103.94),
-        std=(58.393, 57.12, 57.375),
+        preproc_mode='caffe',
+        mean=(123.675, 116.28, 103.53),
+        std=(1., 1., 1.),
         scale=(800, 1333)),
     val=dict(
         type=dataset_type,
@@ -75,9 +113,9 @@ data = dict(
         subset='val',
         flip_ratio=0,
         pad_mode='fixed',
-        preproc_mode='rgb',
-        mean=(123.68, 116.78, 103.94),
-        std=(58.393, 57.12, 57.375),
+        preproc_mode='caffe',
+        mean=(123.675, 116.28, 103.53),
+        std=(1., 1., 1.),
         scale=(800, 1333)),
     test=dict(
         type=dataset_type,
@@ -86,9 +124,9 @@ data = dict(
         subset='val',
         flip_ratio=0,
         pad_mode='fixed',
-        preproc_mode='rgb',
-        mean=(123.68, 116.78, 103.94),
-        std=(58.393, 57.12, 57.375),
+        preproc_mode='caffe',
+        mean=(123.675, 116.28, 103.53),
+        std=(1., 1., 1.),
         scale=(800, 1333)),
 )
 # yapf: enable
@@ -103,7 +141,7 @@ optimizer = dict(
 # extra options related to optimizers
 optimizer_config = dict(
     amp_enabled=True,
-    gradient_clip=5.0, # 15.0,
+    gradient_clip=5.0,
 )
 
 # learning policy
