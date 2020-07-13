dd95a1d0f711793f38e6bb2809004f7ab71ddda4
Jared Nielsen <jaredtnielsen@gmail.com>
2020-07-13 12:07:48 -0700

    WIP preprocess

--- models/nlp/common/preprocess.py
+++ models/nlp/common/preprocess.py
@@ -11,24 +11,26 @@ import random
 from functools import partial
 
 import nlp
-import numpy as np
+from transformers import BertTokenizerFast
 
-dset = nlp.load_dataset("wikitext", "wikitext-2-raw-v1", split="train")
-print(dset, dset[0])
+load_from_cache_file = True
+
+dset = nlp.load_dataset("wikitext", "wikitext-103-raw-v1", split="train")
+print("Loaded dataset:", dset, dset[0])
 dset = dset.filter(lambda ex: len(ex["text"]) > 0)
-print(dset)
+print("Filtered empty lines:", dset, dset[0])
 dset = dset.map(
     lambda batch: {"text": [text.strip().replace("\n", " ") for text in batch["text"]]},
     batched=True,
 )
-print(dset, dset[0])
+print("Replaced newlines with space:", dset, dset[0])
 
 
 def join_documents(batch):
     """ Each document starts with a `= Title =`, and subheadings have two/three equals signs. """
 
 
-def split_sentences(batch):
+def split_into_sentences(batch):
     """ Split into sentences using the '.' separator. Not perfect, converts
 
     Senjō no Valkyria 3 : Unrecorded Chronicles (
@@ -48,26 +50,33 @@ def split_sentences(batch):
     return {"sentences": sentences}
 
 
-dset = dset.map(split_sentences, batched=True, remove_columns=["text"], load_from_cache_file=False)
-print(dset, dset[0])
+dset = dset.map(
+    split_into_sentences,
+    batched=True,
+    remove_columns=["text"],
+    load_from_cache_file=load_from_cache_file,
+)
+print("Split into sentences:", dset, dset[0])
 
-# def group_sentences(batch, target_len: int):
-#     glob = []
-#     cur_str = ""
-#     for ex in batch["sentences"]:
-#         cur_str += ex
-#         if len(cur_str) > target_len:
-#             glob.append(cur_str)
-#             cur_str = ""
-#     glob.append(cur_str)
-#     return {"grouped_sentences": glob}
-
-# dset = dset.map(partial(group_sentences, target_len=5000), batched=True, remove_columns=["sentences"], load_from_cache_file=False)
-# print(dset, dset[0])
+tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
 
 
-def create_example(sentences, max_length, target_length):
+def tokenize(batch):
+
+    return {"tokens": [tokenizer.tokenize(example) for example in batch["sentences"]]}
+
+
+dset = dset.map(tokenize, batched=True, remove_columns=["sentences"])
+print("Tokenized sentences:", dset, dset[0])
+
+# def tokens_to_ids(first_segment, second_segment):
+#     sequence = ["[CLS]"] + first_segment + ["[SEP]"] + second_segment + ["[SEP]"]
+#     return tokenizer.convert_tokens_to_ids(sequence)
+
+
+def create_examples(batch, max_length):
     """Creates a pre-training example from the current list of sentences."""
+    target_length = max_length
     # small chance to only have one segment as in classification tasks
     if random.random() < 0.1:
         first_segment_target_length = 100000
@@ -75,9 +84,9 @@ def create_example(sentences, max_length, target_length):
         # -3 due to not yet having [CLS]/[SEP] tokens in the input text
         first_segment_target_length = (target_length - 3) // 2
 
-    first_segment = []
-    second_segment = []
-    for sentence in sentences:
+    first_segment, second_segment = [], []
+    examples = []
+    for sentence in batch["tokens"]:
         # the sentence goes to the first segment if (1) the first segment is
         # empty, (2) the sentence doesn't put the first segment over length or
         # (3) 50% of the time when it does put the first segment over length
@@ -90,51 +99,49 @@ def create_example(sentences, max_length, target_length):
                 and random.random() < 0.5
             )
         ):
-            first_segment += sentence
+            first_segment += list(sentence)
         else:
-            second_segment += sentence
+            second_segment += list(sentence)
+            if len(first_segment) + len(second_segment) >= target_length:
+                # trim to max_length while accounting for not-yet-added [CLS]/[SEP] tokens
+                first_segment = first_segment[: max_length - 2]
+                second_segment = second_segment[: max(0, max_length - len(first_segment) - 3)]
+                example = ["[CLS]"] + first_segment + ["[SEP]"] + second_segment + ["[SEP]"]
+                examples.append(example)
+                first_segment, second_segment = [], []
 
-    # trim to max_length while accounting for not-yet-added [CLS]/[SEP] tokens
-    first_segment = first_segment[: max_length - 2]
-    second_segment = second_segment[: max(0, max_length - len(first_segment) - 3)]
+                if random.random() < 0.05:
+                    target_length = random.randint(5, max_length)
+                else:
+                    target_length = max_length
 
-    # prepare to start building the next example
-    self._current_sentences = []
-    self._current_length = 0
-    # small chance for random-length instead of max_length-length example
-    if random.random() < 0.05:
-        target_length = random.randint(5, max_length)
-    else:
-        target_length = max_length
+    # This last one may be a little short, but it's necessary to always return something from the function
+    # for the function inspection that only passes two sentences.
+    examples.append(["[CLS]"] + first_segment + ["[SEP]"] + second_segment + ["[SEP]"])
 
-    return f"[CLS]{first_segment}[SEP]{second_segment}[SEP]"
-
-
-def create_examples(batch, n_examples_per_batch: int, max_seq_len: int):
-    examples = []
-    for _ in range(n_examples_per_batch):
-        s1, s2 = np.random.randint(len(batch["sentences"]), size=(2,))
-        text1, text2 = batch["sentences"][s1], batch["sentences"][s2]
-        i1 = np.random.randint(len(text1)) - max_seq_len
-        i2 = np.random.randint(len(text2)) - max_seq_len
-        example = f"[CLS]{text1[i1:i1+max_seq_len]}[SEP]{text2[i2:i2+max_seq_len]}[SEP]"
-        examples.append(example)
     return {"examples": examples}
 
 
-def create_examples_from_sentences(batch, n_examples_per_batch: int, max_seq_len: int):
-    examples = []
-    for _ in range(n_examples_per_batch):
-        pass
+dset = dset.map(
+    partial(create_examples, max_length=512),
+    batched=True,
+    remove_columns=["tokens"],
+    load_from_cache_file=load_from_cache_file,
+)
+print("Created examples:", dset, dset[0])
+
+# TODO: Just use the normal __call__ so it will do the padding for us.
+def tokenize_from_pretokenized(ex):
+    string = tokenizer.decode(tokenizer.convert_tokens_to_ids(ex["examples"]))
+    return tokenizer(string, padding=True, truncation=True, max_length=512)
 
 
 dset = dset.map(
-    partial(create_examples, n_examples_per_batch=1, max_seq_len=128),
-    batched=True,
-    remove_columns=["sentences"],
-    load_from_cache_file=False,
+    tokenize_from_pretokenized,
+    remove_columns=["examples"],
+    cache_file_name="/Users/nieljare/Desktop/wikitext103-encoded.cache",
+    load_from_cache_file=load_from_cache_file,
 )
-print(dset, dset[0])
+print("Padded, truncated, and encoded examples into ids:", dset, dset[0])
 
-
-# breakpoint()
+breakpoint()
