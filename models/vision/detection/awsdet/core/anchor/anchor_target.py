enable low quality samples in RPN, fix ROI scaling in FPN, weight init update

models/vision/detection/awsdet/core/anchor/anchor_target.py
@@ -14,7 +14,8 @@ class AnchorTarget:
                  num_samples=-1, # -1 disables sampling of targets
                  positive_fraction=0.5,
                  pos_iou_thr=0.7,
-                 neg_iou_thr=0.3):
+                 neg_iou_thr=0.3,
+                 allow_low_quality_matches=False):
         """
         Compute regression and classification targets for anchors.
         
@@ -32,6 +33,7 @@ class AnchorTarget:
         self.positive_fraction = positive_fraction
         self.pos_iou_thr = pos_iou_thr
         self.neg_iou_thr = neg_iou_thr
+        self.allow_low_quality_matches = allow_low_quality_matches
 
 
     def get_targets(self,
@@ -135,7 +137,6 @@ class AnchorTarget:
             bbox_outside_weights: (num_anchors, 4)
         """
         gt_bboxes, _ = trim_zeros(gt_bboxes)
-
         # 1. Filter anchors to valid area
         inside_flags = self._anchor_inside_flags(flat_anchors, valid_flags, img_shape)
         # TODO: handle scenario where all flags are False
@@ -150,7 +151,7 @@ class AnchorTarget:
         argmax_overlaps = tf.argmax(overlaps, axis=1, output_type=tf.int32)
         max_overlaps = tf.reduce_max(overlaps, axis=1)
         # b. best anchor index for each GT (non deterministic in case of ties)
-        gt_argmax_overlaps = tf.argmax(overlaps, axis=0, output_type=tf.int32) # tf.where(tf.equal(overlaps, gt_max_overlaps))[:, 0]
+        gt_argmax_overlaps = tf.argmax(overlaps, axis=0, output_type=tf.int32)
 
         # 3. Assign labels
         bg_cond = tf.math.less(max_overlaps, self.neg_iou_thr)
@@ -159,12 +160,24 @@ class AnchorTarget:
         gt_indices = tf.expand_dims(gt_argmax_overlaps, axis=1)
         if gt_labels is None: # RPN will have gt labels set to None
             gt_labels = tf.ones(tf.shape(gt_indices)[0], dtype=tf.int32)
-            #TODO check impact of next 2 lines
-            target_matches = tf.tensor_scatter_nd_update(target_matches, gt_indices, gt_labels) # note that in the case of one label matching multiple anchors the last one wins (is this okay???)
+            target_matches = tf.tensor_scatter_nd_update(target_matches, gt_indices, gt_labels) 
             target_matches = tf.where(fg_cond, tf.ones_like(target_matches), target_matches)
         else:
             gt_labels = gt_labels[:tf.shape(gt_indices)[0]] # get rid of padded labels (-1)
             target_matches = tf.where(fg_cond, tf.gather(gt_labels, argmax_overlaps), target_matches)
+
+        if self.allow_low_quality_matches:
+           # we allow lesser overlap anchors, generally in earlier proposal stages
+           # a. find highest overlap value for each GT (note this max may be lower than pos iou thres)
+           gt_max_overlaps = tf.reduce_max(overlaps, axis=0)
+           # b. assign all anchors that are unassigned but have overlap equal to max overlap for that GT
+           low_quality_matches = tf.math.equal(tf.expand_dims(gt_max_overlaps, 0), overlaps)
+           unassigned_anchors = tf.equal(target_matches, -1)
+           unassigned_matches = tf.where(tf.math.logical_and(tf.expand_dims(unassigned_anchors, -1), low_quality_matches))
+           unassigned_indices = tf.expand_dims(tf.cast(unassigned_matches[:,0], tf.int64), -1)
+           unassigned_labels = tf.gather(gt_labels, unassigned_matches[:,1]) 
+           target_matches = tf.tensor_scatter_nd_update(target_matches,unassigned_indices, unassigned_labels)
+
 
         # 4. Sample selected if we have greater number of candidates than needed by 
         #    config (only if num_samples > 0, e.g. in two stage)

models/vision/detection/awsdet/core/bbox/bbox_target.py
@@ -19,16 +19,15 @@ class ProposalTarget:
                  num_classes=81,
                  reg_class_agnostic=False,
                  fg_assignments=False):
-        '''
+        """
         Compute regression and classification targets for proposals.
         
-        Attributes
-        ---
+        Attributes:
             target_means: [4]. Bounding box refinement mean for RCNN.
             target_stds: [4]. Bounding box refinement standard deviation for RCNN.
             num_rcnn_deltas: int. Maximal number of RoIs per image to feed to bbox heads.
 
-        '''
+        """
         self.target_means = target_means
         self.target_stds = target_stds
         self.num_rcnn_deltas = num_rcnn_deltas
@@ -41,28 +40,26 @@ class ProposalTarget:
         self.fg_assignments = fg_assignments
             
     def build_targets(self, proposals_list, gt_boxes, gt_class_ids, img_metas):
-        '''
-        Generates detection targets for images. Subsamples proposals and
-        generates target class IDs, bounding box deltas for each.
+        """
+        Generate detection targets for images.
+        Subsample proposals and generate target class IDs, bounding box deltas for each.
         
-        Args
-        ---
+        Args:
             proposals_list: list of [num_proposals, (y1, x1, y2, x2)] in regular coordinates.
             gt_boxes: [batch_size, num_gt_boxes, (y1, x1, y2, x2)] in image coordinates.
             gt_class_ids: [batch_size, num_gt_boxes] Integer class IDs.
             img_metas: [batch_size, 11]
             
-        Returns
-        ---
+        Returns:
             rois_list: list of [num_rois, (y1, x1, y2, x2)] in normalized coordinates
             rcnn_target_matchs_list: list of [num_rois]. Integer class IDs.
             rcnn_target_deltas_list: list of [num_positive_rois, (dy, dx, log(dh), log(dw))].
             
         Note that self.num_rcnn_deltas >= num_rois > num_positive_rois. And different 
            images in one batch may have different num_rois and num_positive_rois.
-        '''
+        """
  
-        pad_shapes = calc_pad_shapes(img_metas) # [[1216, 1216]]
+        pad_shapes = calc_pad_shapes(img_metas) 
         batch_size = img_metas.shape[0]
         
         rois_list = []
@@ -97,7 +94,8 @@ class ProposalTarget:
         return (rois_list, rcnn_target_matchs, rcnn_target_deltas, 
                 inside_weights, outside_weights)
 
-    @tf.function(experimental_relax_shapes=True) # relax shapes to reduce function retracing TODO: revisit implementation
+
+    @tf.function(experimental_relax_shapes=True)
     def _build_single_target(self, proposals, gt_boxes, gt_class_ids, img_shape):
         '''
         Args
@@ -118,16 +116,12 @@ class ProposalTarget:
         gt_boxes, non_zeros = trim_zeros(gt_boxes)
         gt_boxes = tf.cast(gt_boxes, proposals.dtype)
         gt_labels = tf.boolean_mask(gt_class_ids, non_zeros)
-        noise_mean = 0.0
-        noisy_gt_boxes = tf.add(gt_boxes, 
-                                tf.random.truncated_normal(tf.shape(gt_boxes), noise_mean, 0.01, dtype=proposals.dtype))
-        proposals_gt = tf.concat([proposals, noisy_gt_boxes], axis=0)
+        proposals_gt = tf.concat([proposals, gt_boxes], axis=0)
 
-
-        iou = geometry.compute_overlaps(proposals_gt, gt_boxes)  # [rois_size, gt_bboxes_size]
-        max_overlaps = tf.reduce_max(iou, axis=1)  # [rois_size, ]
-        gt_assignment = tf.argmax(iou, axis=1)  # [rois_size, ]
-        labels = tf.gather(gt_labels, gt_assignment)  # [rois_size, ]
+        iou = geometry.compute_overlaps(proposals_gt, gt_boxes)
+        max_overlaps = tf.reduce_max(iou, axis=1)
+        gt_assignment = tf.argmax(iou, axis=1)
+        labels = tf.gather(gt_labels, gt_assignment)
 
         # get FG and BG
         fg_inds = tf.where(max_overlaps >= self.pos_iou_thr)[:, 0]

models/vision/detection/awsdet/models/bbox_heads/cascade_head.py
@@ -2,6 +2,7 @@ from .. import builder
 from ..registry import HEADS
 from awsdet.core.bbox import bbox_target, transforms
 from awsdet.models.losses import losses
+from ..utils.misc import calc_img_shapes
 import tensorflow as tf
 
 
@@ -40,7 +41,7 @@ class CascadeHead(tf.keras.Model):
                     num_rcnn_deltas=512,
                     positive_fraction=0.25,
                     pos_iou_thr=iou,
-                    neg_iou_thr=0.1,
+                    neg_iou_thr=0.0,
                     reg_class_agnostic=self.reg_class_agnostic,
                     num_classes=1 if reg_class_agnostic else self.num_classes)
                 self.bbox_targets.append(target)
@@ -64,33 +65,39 @@ class CascadeHead(tf.keras.Model):
         else:
             proposals_list, rcnn_feature_maps, img_metas = inputs
         batch_size = img_metas.shape[0]
+        img_shapes = calc_img_shapes(img_metas)
         loss_dict = {}
         for i in range(self.num_stages):
+            stage_loss_weight = self.stage_loss_weights[i]
             if i == 0:
                 rois_list = proposals_list
             if training:
                 rois_list, rcnn_target_matches, rcnn_target_deltas, inside_weights, \
-                    outside_weights = self.bbox_targets[i].build_targets( \
+                    outside_weights = self.bbox_targets[i].build_targets( 
                     rois_list, gt_boxes, gt_class_ids, img_metas)    
             pooled_regions_list = self.bbox_roi_extractor(
                 (rois_list, rcnn_feature_maps, img_metas), training=training)
             rcnn_class_logits, rcnn_probs, rcnn_deltas = self.bbox_heads[i](pooled_regions_list, training=training)
             if training:
-                loss_dict['rcnn_class_loss_stage_{}'.format(i)] = losses.rcnn_class_loss(rcnn_class_logits, 
-                                                                                         rcnn_target_matches) * self.stage_loss_weights[i]
-        
-                loss_dict['rcnn_box_loss_stage_{}'.format(i)] = losses.rcnn_bbox_loss(rcnn_deltas,
+                loss_dict['s{}_cls_loss'.format(i)] = losses.rcnn_class_loss(rcnn_class_logits,
+                                                                                         rcnn_target_matches) * stage_loss_weight
+                loss_dict['s{}_box_loss'.format(i)] = losses.rcnn_bbox_loss(rcnn_deltas,
                                                                                       rcnn_target_deltas, 
-                                                                                      inside_weights, 
-                                                                                      outside_weights) * self.stage_loss_weights[i]
+                                                                                      inside_weights,
+                                                                                      outside_weights) * stage_loss_weight
             roi_shapes = [tf.shape(i)[0] for i in rois_list]
             refinements = tf.split(rcnn_deltas, roi_shapes)
             new_rois = []
-            if i<(self.num_stages-1):
+            not_last_stage = i < (self.num_stages-1)
+            # box refinement
+            if not_last_stage:
                 for j in range(batch_size):
-                    new_rois.append(tf.stop_gradient(transforms.delta2bbox(rois_list[j], refinements[j],
-                                                   target_means=self.bbox_heads[i].target_means, \
-                                                   target_stds=self.bbox_heads[i].target_stds)))
+                    refined_boxes = transforms.delta2bbox(rois_list[j], refinements[j], target_means=self.bbox_heads[i].target_means, target_stds=self.bbox_heads[i].target_stds)
+                    # Clip to valid area
+                    img_shape = img_shapes[j]
+                    window = tf.stack([0., 0., img_shape[0], img_shape[1]])
+                    refined_boxes = tf.stop_gradient(transforms.bbox_clip(refined_boxes, window))
+                    new_rois.append(refined_boxes)
                 rois_list = new_rois
         if training:
             return loss_dict

models/vision/detection/awsdet/models/roi_extractors/roi_align.py
@@ -39,7 +39,7 @@ def transform_fpcoor_for_tf(boxes, image_shape, crop_shape):
     return tf.concat([ny0, nx0, ny0 + nh, nx0 + nw], axis=1)
 
 @tf.function(experimental_relax_shapes=True)
-def crop_and_resize(image, boxes, box_ind, image_shape, crop_size, pad_border=True):
+def crop_and_resize(image, boxes, box_ind, crop_size, pad_border=True):
     """
     Aligned version of tf.image.crop_and_resize, following our definition of floating point boxes.
     Args:
@@ -58,7 +58,7 @@ def crop_and_resize(image, boxes, box_ind, image_shape, crop_size, pad_border=Tr
         # this can be quite slow
         image = tf.pad(image, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='SYMMETRIC')
         boxes = boxes + 1
-
+    image_shape = tf.shape(image)[1:3]
     boxes = transform_fpcoor_for_tf(boxes, image_shape, [crop_size, crop_size])
     ret = tf.image.crop_and_resize(image, boxes, tf.cast(box_ind, tf.int32),
                                    crop_size=[crop_size, crop_size],
@@ -68,7 +68,7 @@ def crop_and_resize(image, boxes, box_ind, image_shape, crop_size, pad_border=Tr
 
 @ROI_EXTRACTORS.register_module
 class PyramidROIAlign(tf.keras.Model):
-    def __init__(self, pool_shape, pool_type='max', use_tf_crop_and_resize=True, **kwargs):
+    def __init__(self, pool_shape, pool_type='max', **kwargs):
         '''
         Implements ROI Pooling on multiple levels of the feature pyramid.
 
@@ -83,7 +83,6 @@ class PyramidROIAlign(tf.keras.Model):
             self._pool = layers.MaxPool2D(padding='same')
         elif pool_type == 'avg':
             self._pool = layers.AveragePooling2D(padding='same')
-        self.use_tf_crop_and_resize = use_tf_crop_and_resize
 
     @tf.function(experimental_relax_shapes=True)
     def call(self, inputs, training=True):
@@ -118,9 +117,9 @@ class PyramidROIAlign(tf.keras.Model):
 
         # Equation 1 in the Feature Pyramid Networks paper.
         # e.g. a 224x224 ROI (in pixels) maps to P4
-        areas = tf.sqrt(w * h + 1e-8)
+        areas = tf.sqrt(w * h)
         log_2 = tf.cast(tf.math.log(2.), dtype=areas.dtype)
-        roi_levels = tf.floor(4. + tf.math.log(areas / tf.constant(224.0, dtype=areas.dtype)) / log_2)
+        roi_levels = tf.floor(4. + tf.math.log((areas/224.0) + 1e-6) / log_2)
         roi_levels = tf.maximum(roi_levels, tf.ones_like(roi_levels, dtype=roi_levels.dtype) * 2) # min level 2
         roi_levels = tf.minimum(roi_levels, tf.ones_like(roi_levels, dtype=roi_levels.dtype) * 5) # max level 5
         roi_levels = tf.stop_gradient(tf.reshape(roi_levels, [-1]))
@@ -141,29 +140,15 @@ class PyramidROIAlign(tf.keras.Model):
             # Stop gradient propagation to ROI proposals
             level_rois = tf.stop_gradient(level_rois)
             level_roi_indices = tf.stop_gradient(level_roi_indices)
-
-            # Crop and Resize
-            # From Mask R-CNN paper: "We sample four regular locations, so
-            # that we can evaluate either max or average pooling. In fact,
-            # interpolating only a single value at each bin center (without
-            # pooling) is nearly as effective."
-            #
-            # Here we use the simplified approach of a single value per bin,
-            # which is how it's done in tf.crop_and_resize()
-            # Result: [batch * num_rois, pool_height, pool_width, channels]
-            if self.use_tf_crop_and_resize:
-                # normalize rois
-                norm_level_rois = tf.cast(level_rois, tf.float32) / tf.stack([H, W, H, W])
-                crops = tf.image.crop_and_resize(feature_map_list[i], norm_level_rois,
-                                             box_indices=level_roi_indices,
-                                             crop_size=self.pre_pool_shape,
-                                             method='bilinear')
-            else:
-                crops = crop_and_resize(feature_map_list[i],
-                                    level_rois,
+            featmap_shape = tf.shape(feature_map_list[i])[1:3]
+            feat_h = tf.cast(featmap_shape[0], tf.float32)
+            feat_w = tf.cast(featmap_shape[1], tf.float32)
+            norm_level_rois = tf.cast(level_rois, tf.float32) / tf.stack([H, W, H, W]) * tf.stack([feat_h, feat_w, feat_h, feat_w])
+            crops = crop_and_resize(feature_map_list[i],
+                                    norm_level_rois,
                                     level_roi_indices,
-                                    [H, W],
                                     self.pre_pool_shape[0])
+
             crops = self._pool(crops)
             pooled_rois.append(crops)
 