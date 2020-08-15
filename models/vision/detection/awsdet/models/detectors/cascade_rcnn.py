enable low quality samples in RPN, fix ROI scaling in FPN, weight init update

models/vision/detection/awsdet/models/detectors/cascade_rcnn.py
@@ -43,8 +43,8 @@ class CascadeRCNN(TwoStageDetector):
                     # check if backbone has weights
                     self.backbone.init_weights()
         else:
-            #_, extension = os.path.splitext(self.pretrained)
-            self.load_weights(self.pretrained) # , by_name=True)
+            self.load_weights(self.pretrained)
+
 
     @tf.function(experimental_relax_shapes=True)
     def call(self, inputs, training=True, use_dali=False):
@@ -88,3 +88,4 @@ class CascadeRCNN(TwoStageDetector):
             cascade_inputs = (proposals_list, rcnn_feature_maps, img_metas)
             cascade_predictions = self.bbox_head(cascade_inputs, training=training)
             return cascade_predictions
+

models/vision/detection/awsdet/models/necks/fpn.py
@@ -11,6 +11,7 @@ FPN model used by Faster R-CNN and RetinaNet.
 '''
 import tensorflow as tf
 from tensorflow.keras import layers
+from tensorflow.keras.initializers import VarianceScaling
 from ..registry import NECKS
 
 @NECKS.register_module
@@ -64,12 +65,12 @@ class FPN(tf.keras.Model):
             l_conv = layers.Conv2D(out_channels, lat_kernel_size, strides=1, use_bias=use_bias,
                                     name='lateral_{}'.format(channel_name),
                                     kernel_regularizer=tf.keras.regularizers.l2(weight_decay),
-                                    padding='same', kernel_initializer='glorot_uniform')
+                                    padding='same', kernel_initializer=VarianceScaling(distribution='uniform'))
             fpn_kernel_size = 3
             fpn_conv = layers.Conv2D(out_channels, fpn_kernel_size, 1, use_bias=use_bias,
                                        kernel_regularizer=tf.keras.regularizers.l2(weight_decay),
                                        padding='same', name='fpn_{}'.format(channel_name),
-                                       kernel_initializer='glorot_uniform')
+                                       kernel_initializer=VarianceScaling(distribution='uniform'))
             self.lateral_convs.append(l_conv)
             self.fpn_convs.append(fpn_conv)
             if i > self.start_level:
@@ -83,7 +84,7 @@ class FPN(tf.keras.Model):
                                                 use_bias=use_bias,
                                                 kernel_regularizer=tf.keras.regularizers.l2(weight_decay),
                                                 name='extra_conv_{}'.format(i),
-                                                kernel_initializer='glorot_uniform')
+                                                kernel_initializer=VarianceScaling(distribution='uniform'))
                 self.fpn_convs.append(extra_fpn_conv)
         self._method = interpolation_method
 
@@ -106,9 +107,7 @@ class FPN(tf.keras.Model):
             laterals[i-1] = layers.Add()([laterals[i-1], upsampled])
 
         # outputs
-        outs = [
-            self.fpn_convs[i](laterals[i]) for i in range(used_backbone_levels)
-        ]        
+        outs = [self.fpn_convs[i](laterals[i]) for i in range(used_backbone_levels)]        
 
         # extra levels
         if self.num_outs > len(outs):