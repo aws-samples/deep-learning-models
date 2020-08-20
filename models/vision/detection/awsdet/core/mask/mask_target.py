e700df88fb50a11b454d08794ecbf10a9209ae1b
Anurag Singh <anuragrs@gmail.com>
2020-08-20 04:28:03 +0000

    Mask R-CNN fixes, almost SoTA bbox_mAP_copypaste: 0.376 0.584 0.416 0.229 0.406 0.480, segm_mAP_copypaste: 0.344 0.553 0.369 0.190 0.372 0.460

--- models/vision/detection/awsdet/core/mask/mask_target.py
+++ models/vision/detection/awsdet/core/mask/mask_target.py
@@ -3,6 +3,65 @@
 # -*- coding: utf-8 -*-
 import tensorflow as tf
 
+# modification of aligned crop and resize from tensorpack
+@tf.function(experimental_relax_shapes=True)
+def transform_fpcoor_for_tf(boxes, image_shape, crop_shape):
+    """
+    The way tf.image.crop_and_resize works (with normalized box):
+    Initial point (the value of output[0]): x0_box * (W_img - 1)
+    Spacing: w_box * (W_img - 1) / (W_crop - 1)
+    Use the above grid to bilinear sample.
+    However, what we want is (with fpcoor box):
+    Spacing: w_box / W_crop
+    Initial point: x0_box + spacing/2 - 0.5
+    (-0.5 because bilinear sample (in my definition) assumes floating point coordinate
+     (0.0, 0.0) is the same as pixel value (0, 0))
+    This function transform fpcoor boxes to a format to be used by tf.image.crop_and_resize
+    Returns:
+        y1x1y2x2
+    """
+    y0, x0, y1, x1 = tf.split(boxes, 4, axis=1)
+
+    spacing_w = (x1 - x0) / tf.cast(crop_shape[1], tf.float32)
+    spacing_h = (y1 - y0) / tf.cast(crop_shape[0], tf.float32)
+
+    imshape = [tf.cast(image_shape[0]-1, tf.float32), tf.cast(image_shape[1]-1, tf.float32)]
+    nx0 = (x0 + spacing_w / 2 - 0.5) / imshape[1]
+    ny0 = (y0 + spacing_h / 2 - 0.5) / imshape[0]
+
+    nw = spacing_w * tf.cast(crop_shape[1] - 1, tf.float32) / imshape[1]
+    nh = spacing_h * tf.cast(crop_shape[0] - 1, tf.float32) / imshape[0]
+
+    return tf.concat([ny0, nx0, ny0 + nh, nx0 + nw], axis=1)
+
+@tf.function(experimental_relax_shapes=True)
+def crop_and_resize(image, boxes, box_ind, crop_size, pad_border=True):
+    """
+    Aligned version of tf.image.crop_and_resize, following our definition of floating point boxes.
+    Args:
+        image: NHWC
+        boxes: nx4, y1x1y2x2
+        box_ind: (n,)
+        crop_size (int):
+    Returns:
+        n,C,size,size
+    """
+    assert isinstance(crop_size, int), crop_size
+    boxes = tf.stop_gradient(boxes)
+
+    # TF's crop_and_resize produces zeros on border
+    if pad_border:
+        # this can be quite slow
+        image = tf.pad(image, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='SYMMETRIC')
+        boxes = boxes + 1
+    image_shape = tf.shape(image)[1:3]
+    boxes = transform_fpcoor_for_tf(boxes, image_shape, [crop_size, crop_size])
+    ret = tf.image.crop_and_resize(image, boxes, tf.cast(box_ind, tf.int32),
+                                   crop_size=[crop_size, crop_size],
+                                   method='bilinear')
+    return ret
+
+
 class MaskTarget:
 
     def __init__(self, max_fg=128,
@@ -73,12 +132,12 @@ class MaskTarget:
         W = tf.shape(gt_masks)[3]
         gt_masks = tf.reshape(gt_masks, [-1, H, W, 1])
         fg_rois = tf.concat(fg_rois_list, axis=0)
-        norm_fg_rois = fg_rois / tf.cast(tf.stack([H-1, W-1, H-1, W-1]), fg_rois.dtype)
-        crops = tf.image.crop_and_resize(image=gt_masks,
-                                         boxes=norm_fg_rois,
-                                         box_indices=fg_offsets,
-                                         crop_size=self.mask_size,
-                                         method='bilinear')
+        norm_fg_rois = fg_rois #/ tf.cast(tf.stack([H-1, W-1, H-1, W-1]), fg_rois.dtype)
+        crops = crop_and_resize(gt_masks,
+                                norm_fg_rois,
+                                fg_offsets,
+                                self.mask_size[0],
+                                pad_border=False)
         return crops
 
     def get_mask_targets(self, gt_masks, fg_assignments, rcnn_target_matchs, fg_rois_list, img_metas):
